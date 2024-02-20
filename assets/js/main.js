948d06f1aef7fca8b985ebd2bda3e2584d71cd21
antden99 <antoniodenigris@icloud.com>
2024-02-20 15:54:15 +0100

    final mailestone 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,8 +21,33 @@ document.querySelector("button").addEventListener("click", function () {
     let ageUpdate = Number(age.value);
     let distanceUpdate = Number(distance.value);
 
+    // Calcolo il prezzo del biglietto, moltiplicando la distanza da percorrere per il prezzo a km
+    let price= ( distanceUpdate * 0.21);
+    console.log(price);
+
+    //Applico uno sconto se vengono rispettate determinate condizioni, altrimenti no
+    let final;
+    if (ageUpdate < 18) {
+        final = price - (price * 0.20);
+        final = final.toFixed(2);
+        console.log(`il prezzo del biglietto con lo sconto del 20% applicato è di €${final}`);
+        
+    
+    } else if (ageUpdate > 65) {
+        final = price - (price * 0.40);
+        final = final.toFixed(2);
+        console.log(`il prezzo del biglietto con lo sconto del 40% applicato è di €${final}`);
+        
+    
+    } else {
+        console.log(price);
+        final = price.toFixed(2);
+        document.writeln(`Il prezzo del biglietto  è di €${final}`);
+    }
     
 })
+
+
 
 
 
