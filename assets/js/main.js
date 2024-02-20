ef92f2458103922ef520f0e6b74df28e9d1e69df
antden99 <antoniodenigris@icloud.com>
2024-02-20 14:31:45 +0100

    add input and button
    aggiungo l'html della prima mileston

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
 console.log("Funziona");
+
+const name = document.getElementById("name");
+console.log(name.value);
+
+
+const age = document.getElementById("age");
+console.log(age.value);
+
+document.querySelector("button").addEventListener("click", function(){
+    console.log(name.value, age.value);
+})
